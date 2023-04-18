e6b6f316ab7df32d91d4b748f687eba19717dff9
Igor <qfrage@gmail.com>
2023-04-18 14:33:34 +0300

    formatting code

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(session({
     secret: 'mysecret', // Секрет для подписания куки
     resave: false, // Не сохранять сессию, если она не изменилась
     saveUninitialized: false // Не сохранять пустую сессию
-  }));
+}));
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -23,48 +23,48 @@ connection.connect((err) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/login_page/index.html');
+    res.sendFile(__dirname + '/login_page/index.html');
 });
 app.get('/script.js', (req, res) => {
     res.sendFile(__dirname + '/login_page/script.js');
 });
 
-  
-  app.get('/panel/main_page', (req, res) => {
+
+app.get('/panel/main_page', (req, res) => {
     if (req.session.isAuthenticated) {
-      res.sendFile(__dirname + '/panel/main_page.html');
+        res.sendFile(__dirname + '/panel/main_page.html');
     } else {
-      res.redirect('/');
+        res.redirect('/');
     }
-  });
-  
+});
+
 app.get('/panel/script.js', (req, res) => {
     res.sendFile(__dirname + '/panel/script.js');
-  });
-  
-app.post('/login', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  connection.query(
-    'SELECT * FROM admins WHERE login = ? AND password = ?',
-    [username, password],
-    (error, results, fields) => {
-      if (error) throw error;
-
-      if (results.length > 0) {
-        req.session.isAuthenticated = true; // Устанавливаем флаг аутентификации в сессии
-        res.redirect('/panel/main_page');
-        // Действия, которые нужно выполнить в случае успешной аутентификации
-      } else {
-        res.send('Неверный логин или пароль');
-        // Действия, которые нужно выполнить в случае ошибки аутентификации
-      }
-    }
-  );
 });
-  
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    connection.query(
+        'SELECT * FROM admins WHERE login = ? AND password = ?',
+        [username, password],
+        (error, results, fields) => {
+            if (error) throw error;
+
+            if (results.length > 0) {
+                req.session.isAuthenticated = true; // Устанавливаем флаг аутентификации в сессии
+                res.redirect('/panel/main_page');
+                // Действия, которые нужно выполнить в случае успешной аутентификации
+            } else {
+                res.send('Неверный логин или пароль');
+                // Действия, которые нужно выполнить в случае ошибки аутентификации
+            }
+        }
+    );
+});
+
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+    console.log('Server started on port 3000');
 });
