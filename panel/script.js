2fb587343b3edc1b7d453fd72bd978b04b98eada
Igor <qfrage@gmail.com>
2023-06-15 02:25:46 +0300

    beautify code

--- panel/script.js
+++ panel/script.js
@@ -42,13 +42,13 @@ function openDialogEditFAQ() {
     // Поле введення
     var inputField = document.createElement('input');
     inputField.classList.add('dialog-input');
-    if(this.question)inputField.value = this.question;
+    if (this.question) inputField.value = this.question;
     else inputField.placeholder = 'Питання'
 
     // Зона для введення великого тексту
     var textareaField = document.createElement('textarea');
     textareaField.classList.add('dialog-textarea');
-    if(this.answer)textareaField.value = this.answer;
+    if (this.answer) textareaField.value = this.answer;
     else textareaField.placeholder = 'Відповідь'
 
     // Кнопка зберегти
@@ -56,12 +56,12 @@ function openDialogEditFAQ() {
     saveButton.classList.add('dialog-save');
     saveButton.textContent = 'Зберегти';
     var sql_id = this.faq_id
-    saveButton.onclick = function(){
+    saveButton.onclick = function () {
         // Отримання значень полів введення
         const titleInput = document.querySelector('.dialog-input');
         const contentTextarea = document.querySelector('.dialog-textarea');
         const f_id = sql_id; // Ідентифікатор запису, який потрібно оновити
-    
+
         // Перевірка наявності значень
         if (titleInput.value && contentTextarea.value) {
             // Відправка HTTP-запиту на сервер
@@ -76,15 +76,15 @@ function openDialogEditFAQ() {
                     content: contentTextarea.value,
                 }),
             })
-            .then((response) => response.text())
-            .then((result) => {
-                showPopup(result); // Вивід результату в консоль
-                dialogOverlay.remove();
-                getFAQs();
-            })
-            .catch((error) => {
-                showPopup('Сталася помилка:'+ error);
-            });
+                .then((response) => response.text())
+                .then((result) => {
+                    showPopup(result); // Вивід результату в консоль
+                    dialogOverlay.remove();
+                    getFAQs();
+                })
+                .catch((error) => {
+                    showPopup('Сталася помилка:' + error);
+                });
         } else {
             showPopup('Не всі поля заповнені');
         }
@@ -94,10 +94,10 @@ function openDialogEditFAQ() {
     var closeButton = document.createElement('span');
     closeButton.classList.add('dialog-close');
     closeButton.textContent = 'Закрити';
-    
-    closeButton.onclick = function() {
-      // Закриття діалогового вікна
-      dialogOverlay.remove();
+
+    closeButton.onclick = function () {
+        // Закриття діалогового вікна
+        dialogOverlay.remove();
     };
 
     // Додавання елементів до діалогового вікна
@@ -111,9 +111,9 @@ function openDialogEditFAQ() {
     // Додавання діалогового вікна до DOM
     dialogOverlay.appendChild(dialogBox);
     document.body.appendChild(dialogOverlay);
-  }
+}
 
-  function fillTableUsers(data) {
+function fillTableUsers(data) {
     var table = document.querySelector('#users .users_table');
 
     // Очищуємо існуючі значення
@@ -141,22 +141,22 @@ function openDialogEditFAQ() {
 
     // Створюємо рядки з даними
     for (var i = 0; i < data.length; i++) {
-      var rowData = data[i];
-      var row = document.createElement('tr');
-      var name = document.createElement('td');
-      var blocked = document.createElement('td');
-      var admin = document.createElement('td');
-      var telegram_id = document.createElement('td')
+        var rowData = data[i];
+        var row = document.createElement('tr');
+        var name = document.createElement('td');
+        var blocked = document.createElement('td');
+        var admin = document.createElement('td');
+        var telegram_id = document.createElement('td')
 
-      name.classList.add('users_name')
-      blocked.classList.add('users_blocked')
-      admin.classList.add('users_admin');
-      telegram_id.classList.add('users_telegram_id')
-      name.textContent = rowData.user_name;
-      blocked.textContent = rowData.user_blocked
-      admin.textContent = rowData.user_admin
-      telegram_id.textContent = rowData.user_telegram_id; 
-      row.appendChild(name);
+        name.classList.add('users_name')
+        blocked.classList.add('users_blocked')
+        admin.classList.add('users_admin');
+        telegram_id.classList.add('users_telegram_id')
+        name.textContent = rowData.user_name;
+        blocked.textContent = rowData.user_blocked
+        admin.textContent = rowData.user_admin
+        telegram_id.textContent = rowData.user_telegram_id;
+        row.appendChild(name);
         row.appendChild(blocked);
         row.appendChild(admin);
         row.appendChild(telegram_id);
@@ -164,7 +164,7 @@ function openDialogEditFAQ() {
         table.appendChild(row);
     }
     const user_sum_el = document.getElementById('users_sum')
-    user_sum_el.textContent='Всього: '+data.length
+    user_sum_el.textContent = 'Всього: ' + data.length
 }
 
 
@@ -186,32 +186,32 @@ function fillTableFAQ(data) {
 
     // Створюємо рядки з даними
     for (var i = 0; i < data.length; i++) {
-      var rowData = data[i];
-      var row = document.createElement('tr');
-      var cell1 = document.createElement('td');
-      var cell2 = document.createElement('td');
-      var edit_btn = document.createElement('td');
-      var remove_btn = document.createElement('td');
+        var rowData = data[i];
+        var row = document.createElement('tr');
+        var cell1 = document.createElement('td');
+        var cell2 = document.createElement('td');
+        var edit_btn = document.createElement('td');
+        var remove_btn = document.createElement('td');
 
-      cell1.classList.add('faq_title')
-      cell2.classList.add('faq_content')
-      edit_btn.classList.add('faq_edit_btn')
-      remove_btn.classList.add('faq_remove_btn');
-      cell1.textContent = rowData.question;
-      cell2.textContent = rowData.answer;
-      edit_btn.faq_id = rowData.sql_id;
-      edit_btn.answer = rowData.answer;
-      edit_btn.question = rowData.question; 
-      edit_btn.textContent = 'Редагувати'
-      edit_btn.addEventListener('click', openDialogEditFAQ);
-      remove_btn.textContent = 'Видалити'
-      remove_btn.faq_id = rowData.sql_id;
-      remove_btn.onclick = function(){
-        var faqId = this.faq_id;
-    
-        if (faqId) {
-            // Відправка HTTP-запиту на сервер
-            fetch('/deleteFaq', {
+        cell1.classList.add('faq_title')
+        cell2.classList.add('faq_content')
+        edit_btn.classList.add('faq_edit_btn')
+        remove_btn.classList.add('faq_remove_btn');
+        cell1.textContent = rowData.question;
+        cell2.textContent = rowData.answer;
+        edit_btn.faq_id = rowData.sql_id;
+        edit_btn.answer = rowData.answer;
+        edit_btn.question = rowData.question;
+        edit_btn.textContent = 'Редагувати'
+        edit_btn.addEventListener('click', openDialogEditFAQ);
+        remove_btn.textContent = 'Видалити'
+        remove_btn.faq_id = rowData.sql_id;
+        remove_btn.onclick = function () {
+            var faqId = this.faq_id;
+
+            if (faqId) {
+                // Відправка HTTP-запиту на сервер
+                fetch('/deleteFaq', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -220,14 +220,14 @@ function fillTableFAQ(data) {
                         id: faqId
                     }),
                 })
-                .then((response) => response.text())
-                .then((result) => {
-                    showPopup(result); // Вивід результату в консоль
-                    getFAQs(); // Оновлення таблиці після видалення
-                })
-                .catch((error) => {
-                    console.error('Помилка:', error);
-                });
+                    .then((response) => response.text())
+                    .then((result) => {
+                        showPopup(result); // Вивід результату в консоль
+                        getFAQs(); // Оновлення таблиці після видалення
+                    })
+                    .catch((error) => {
+                        console.error('Помилка:', error);
+                    });
             } else {
                 console.error('Ідентифікатор не визначений');
             }
@@ -239,46 +239,46 @@ function fillTableFAQ(data) {
         table.appendChild(row);
     }
     const faq_sum_el = document.getElementById('faq_sum')
-    faq_sum_el.textContent='Всього питань: '+data.length
+    faq_sum_el.textContent = 'Всього питань: ' + data.length
     allFAQs = data.length;
 }
 
-function getUsers(){
+function getUsers() {
     var dataArray = [
     ];
     fetch('/api/users')
-    .then(response => response.json())
-    .then(data => {
-      // Обробка отриманої JSON-відповіді
-      data.forEach(element => {
-          dataArray.push({user_telegram_id:element.user_id,user_name:element.username,user_blocked:element.is_blocked,user_admin:element.is_admin});
-      });
-      // Викликаємо функцію для заповнення таблиці
-      fillTableUsers(dataArray);   
-    })
-    .catch(error => {
-      // Обробка помилок
-      console.error('Помилка при отриманні даних:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            // Обробка отриманої JSON-відповіді
+            data.forEach(element => {
+                dataArray.push({ user_telegram_id: element.user_id, user_name: element.username, user_blocked: element.is_blocked, user_admin: element.is_admin });
+            });
+            // Викликаємо функцію для заповнення таблиці
+            fillTableUsers(dataArray);
+        })
+        .catch(error => {
+            // Обробка помилок
+            console.error('Помилка при отриманні даних:', error);
+        });
 }
 
-function getFAQs(){
+function getFAQs() {
     var dataArray = [
     ];
     fetch('/api/faq')
-    .then(response => response.json())
-    .then(data => {
-      // Обробка отриманої JSON-відповіді
-      data.forEach(element => {
-          dataArray.push({sql_id:element.id,question:element.title,answer:element.content});
-      });
-      // Викликаємо функцію для заповнення таблиці
-      fillTableFAQ(dataArray);   
-    })
-    .catch(error => {
-      // Обробка помилок
-      console.error('Помилка при отриманні даних:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            // Обробка отриманої JSON-відповіді
+            data.forEach(element => {
+                dataArray.push({ sql_id: element.id, question: element.title, answer: element.content });
+            });
+            // Викликаємо функцію для заповнення таблиці
+            fillTableFAQ(dataArray);
+        })
+        .catch(error => {
+            // Обробка помилок
+            console.error('Помилка при отриманні даних:', error);
+        });
 }
 
 // Прохід по елементам tablinks і додавання класу active для поточного/клікнутого tablink
@@ -286,7 +286,7 @@ for (const tabLink of tabLinks) {
     tabLink.addEventListener("click", function (e) {
         e.preventDefault();
         const tabId = this.getAttribute("href").substring(1);
-        
+
 
         // Видалення класу active з усіх tablink
         for (const tabLink of tabLinks) {
@@ -303,10 +303,10 @@ for (const tabLink of tabLinks) {
 
         // Показ обраного tabcontent
         document.getElementById(tabId).classList.add("active");
-        if(tabId == "faqs"){
-            getFAQs();  
+        if (tabId == "faqs") {
+            getFAQs();
         }
-        else if(tabId == 'users'){
+        else if (tabId == 'users') {
             getUsers();
         }
     });
@@ -316,8 +316,8 @@ for (const tabLink of tabLinks) {
 var addButton = document.querySelector('.add-button');
 
 // Додаємо обробник події для кнопки
-addButton.addEventListener('click', function() {
-  openDialogEditFAQ();
+addButton.addEventListener('click', function () {
+    openDialogEditFAQ();
 });
 
 
@@ -325,105 +325,105 @@ addButton.addEventListener('click', function() {
 const inputField = document.querySelector('.input-field');
 const inputFieldUsers = document.querySelector('.input-field-users')
 
-inputFieldUsers.addEventListener('input', function(event) {
+inputFieldUsers.addEventListener('input', function (event) {
     const inputValue = event.target.value.toLowerCase(); // Перетворюємо введене значення в нижній регістр
     const faqRows = document.querySelectorAll('.users_table tr'); // Отримуємо всі теги <tr> в межах таблиці
-  
+
     // Перебираємо теги <tr>
     for (const faqRow of faqRows) {
-      const userName = faqRow.querySelector('.users_name'); 
-      const userTelegram = faqRow.querySelector('.users_telegram_id')
-      if (userName && userTelegram) {
-        const nameText = userName.textContent.toLowerCase(); // Отримуємо текст тегу
-        const telegramText = userTelegram.textContent.toLowerCase();
-        // Перевіряємо, чи містить текст тегів введений символ
-        if (nameText.includes(inputValue) || telegramText.includes(inputValue)) {
-            faqRow.style.display = 'table-row'; // Відображаємо родительський тег <tr>, якщо знайдено співпадіння
-        } else {
-            faqRow.style.display = 'none'; // Приховуємо родительський тег <tr>, якщо не знайдено співпадіння
+        const userName = faqRow.querySelector('.users_name');
+        const userTelegram = faqRow.querySelector('.users_telegram_id')
+        if (userName && userTelegram) {
+            const nameText = userName.textContent.toLowerCase(); // Отримуємо текст тегу
+            const telegramText = userTelegram.textContent.toLowerCase();
+            // Перевіряємо, чи містить текст тегів введений символ
+            if (nameText.includes(inputValue) || telegramText.includes(inputValue)) {
+                faqRow.style.display = 'table-row'; // Відображаємо родительський тег <tr>, якщо знайдено співпадіння
+            } else {
+                faqRow.style.display = 'none'; // Приховуємо родительський тег <tr>, якщо не знайдено співпадіння
+            }
         }
-      }
     }
-  });
+});
 // Додаємо обробник події введення
-inputField.addEventListener('input', function(event) {
+inputField.addEventListener('input', function (event) {
     const inputValue = event.target.value.toLowerCase(); // Перетворюємо введене значення в нижній регістр
     const faqRows = document.querySelectorAll('.faq_table tr'); // Отримуємо всі теги <tr> в межах таблиці
-  
+
     // Перебираємо теги <tr>
     for (const faqRow of faqRows) {
-      const faqTitle = faqRow.querySelector('.faq_title'); // Отримуємо тег <td class="faq_title">
-      const faqContent = faqRow.querySelector('.faq_content'); // Отримуємо тег <td class="faq_content">
-  
-      if (faqTitle && faqContent) {
-        const titleText = faqTitle.textContent.toLowerCase(); // Отримуємо текст тегу .faq_title в нижньому регістрі
-        const contentText = faqContent.textContent.toLowerCase(); // Отримуємо текст тегу .faq_content в нижньому регістрі
-  
-        // Перевіряємо, чи містить текст тегів введений символ
-        if (titleText.includes(inputValue) || contentText.includes(inputValue)) {
-          faqRow.style.display = 'table-row'; // Відображаємо родительський тег <tr>, якщо знайдено співпадіння
-        } else {
-          faqRow.style.display = 'none'; // Приховуємо родительський тег <tr>, якщо не знайдено співпадіння
-        }
-      }
-    }
-  });
+        const faqTitle = faqRow.querySelector('.faq_title'); // Отримуємо тег <td class="faq_title">
+        const faqContent = faqRow.querySelector('.faq_content'); // Отримуємо тег <td class="faq_content">
 
-  function checkUpdateInDB() {
+        if (faqTitle && faqContent) {
+            const titleText = faqTitle.textContent.toLowerCase(); // Отримуємо текст тегу .faq_title в нижньому регістрі
+            const contentText = faqContent.textContent.toLowerCase(); // Отримуємо текст тегу .faq_content в нижньому регістрі
+
+            // Перевіряємо, чи містить текст тегів введений символ
+            if (titleText.includes(inputValue) || contentText.includes(inputValue)) {
+                faqRow.style.display = 'table-row'; // Відображаємо родительський тег <tr>, якщо знайдено співпадіння
+            } else {
+                faqRow.style.display = 'none'; // Приховуємо родительський тег <tr>, якщо не знайдено співпадіння
+            }
+        }
+    }
+});
+
+function checkUpdateInDB() {
     // Ваш код або дії, які потрібно виконати
     const tab = document.getElementById('faqs');
-    if(tab.classList.contains("active")){
+    if (tab.classList.contains("active")) {
         fetch('/api/faq')
-        .then(response => response.json())
-        .then(data => {
-        // Обробка отриманої JSON-відповіді
-        if(allFAQs != data.length){
-            showPopup("База даних оновилась!");
-            allFAQs = data.length;
-            getFAQs();
-        }   
-        })
-        .catch(error => {
-        // Обробка помилок
-        console.error('Помилка при отриманні даних:', error);
-        });
+            .then(response => response.json())
+            .then(data => {
+                // Обробка отриманої JSON-відповіді
+                if (allFAQs != data.length) {
+                    showPopup("База даних оновилась!");
+                    allFAQs = data.length;
+                    getFAQs();
+                }
+            })
+            .catch(error => {
+                // Обробка помилок
+                console.error('Помилка при отриманні даних:', error);
+            });
     }
 }
-  
+
 // Виклик функції кожні 5 секунд
 setInterval(checkUpdateInDB, 5000);
 
 function showPopup(text) {
     const popupContainer = document.createElement('div');
     const popupText = document.createElement('p');
-  
+
     popupText.textContent = text;
-  
+
     popupContainer.classList.add('popup-container');
     popupText.classList.add('popup-text');
-  
+
     popupContainer.appendChild(popupText);
-  
+
     document.body.appendChild(popupContainer);
-  
+
     // Перемещаем существующие окна вниз
     const existingPopups = document.querySelectorAll('.popup-container');
     existingPopups.forEach((popup, index) => {
-      popup.style.transform = `translateY(${index * 80}px)`;
+        popup.style.transform = `translateY(${index * 80}px)`;
     });
-  
+
     // Закрытие окна через 3 секунды
-    setTimeout(function() {
-      // Удаляем окно после окончания анимации
-      popupContainer.classList.add('popup-fade-out');
-      setTimeout(function() {
-        document.body.removeChild(popupContainer);
-  
-        // Восстанавливаем позиции существующих окон
-        const remainingPopups = document.querySelectorAll('.popup-container');
-        remainingPopups.forEach((popup, index) => {
-          popup.style.transform = `translateY(${index * 80}px)`;
-        });
-      }, 500); // Задержка 500 миллисекунд для завершения анимации
+    setTimeout(function () {
+        // Удаляем окно после окончания анимации
+        popupContainer.classList.add('popup-fade-out');
+        setTimeout(function () {
+            document.body.removeChild(popupContainer);
+
+            // Восстанавливаем позиции существующих окон
+            const remainingPopups = document.querySelectorAll('.popup-container');
+            remainingPopups.forEach((popup, index) => {
+                popup.style.transform = `translateY(${index * 80}px)`;
+            });
+        }, 500); // Задержка 500 миллисекунд для завершения анимации
     }, 3000); // Закрытие окна через 3 секунды
-  }
+}
